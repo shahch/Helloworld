352e0ca7ef6910441dc1ec35cc4cdc9cc9c34042
shahch <cshah4195@gmail.com>
2014-09-16 12:35:03 -0400

    unittest
    First unit test

--- hello_pyramid/tests.py
+++ hello_pyramid/tests.py
@@ -1,17 +1,23 @@
 import unittest
 
+from webtest import TestApp
+
+from pyramid.paster import get_app
 from pyramid import testing
 
 
 class ViewTests(unittest.TestCase):
+
     def setUp(self):
-        self.config = testing.setUp()
+       app = get_app('testing.ini')
+       self.testapp = TestApp(app)
 
     def tearDown(self):
-        testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'hello_pyramid')
+       del self.testapp
+       #self.driver.close()
+    def test_root(self):
+       res = self.testapp.get('/')
+       # <h1><span class="font-semi-bold">Hello</span> <span class="font-semi-bold">world</span></h1><span class="small">By Chirag Shah</span>
+       self.assertTrue(b' <h1><span class="font-semi-bold">Hello</span> <span class="font-semi-bold">world</span></h1><span class="small">By Chirag Shah</span>' in res.body) #Make sure there is remember me checkbox
+       
